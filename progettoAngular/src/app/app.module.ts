bbfcdffa6ad46df29f5df56637c825e7886539de
mattia2312 <mattyc753@gmail.com>
2024-06-26 17:16:58 +0200

    commit

--- progettoAngular/src/app/app.module.ts
+++ progettoAngular/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {} from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RegistrazioneComponent } from './Component/Registrazione/registrazione.component';
@@ -55,6 +55,7 @@ import {MatTooltipModule} from '@angular/material/tooltip';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
     ReactiveFormsModule,
     FormsModule,
     BrowserAnimationsModule,
